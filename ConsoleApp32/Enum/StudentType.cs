bb8b439c80d0feb85cd01145fbdcd4a40988cdbb
allahshukur <Allahshukurjg@code.edu.az>
2022-03-09 09:25:21 +0400

    first commit

---
+++ ConsoleApp32/Enum/StudentType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp32.Enum
+{
+    enum StudentType
+    {
+        Waranty = 1,
+        WithoutWaraty
+    }
+}
---
+++ ConsoleApp32/Student/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleApp32.Enum;
+
+namespace ConsoleApp32.Student
+{
+    internal class Student
+    {
+        public string Fullname { get; set; }
+
+        public int GroupNo { get; set; }
+        
+        public StudentType StudentType { get; set; }
+        public bool IsOnline { get; private set; }
+
+        public Student(string fullname , int groupNo , StudentType studentType)
+        {
+            Fullname = fullname;
+            GroupNo = groupNo;
+            StudentType = studentType;
+        }
+
+        public override string ToString()
+        {
+            string isWaranty = (int)StudentType == 1 ? "zenametli" : "zemanetsiz";
+            string isOnline = IsOnline ? "Online" : "Ofline";
+
+            return $"Ad Soyad: {Fullname}\nQroup nomresi: {GroupNo}\nZemanet Novu: {isWaranty}";
+        }
+
+    }
+
+}
